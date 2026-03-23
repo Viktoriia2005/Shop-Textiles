af26ccb17dd86d3855c8f392f07f3d97db769fd2
Vivi <vika.pavlenko.161105@gmail.com>
2026-03-23 20:51:23 +0200

    Connecting a new database.

--- Server/db.js
+++ Server/db.js
@@ -5,7 +5,7 @@ export const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '', // ← порожній рядок
-    database: 'pavlenko_shop',
+    database: 'textiles_p',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
